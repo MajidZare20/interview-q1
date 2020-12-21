dc12a8a83711cd98f5a61cfb8002ee02088e2817
MajidZare20 <76279339+MajidZare20@users.noreply.github.com>
2020-12-21 12:20:35 +0330

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,29 +1,36 @@
 #include <stdio.h>
 #include <time.h>
 #include <math.h>
- 
+
 int prop(long int A, long int B, long int C, long int D) {
     return (pow(A, 4) + pow(B, 4) + pow(C, 4) == pow(D, 4));
 }
- 
+
 int main() {
     long int a, b, c, d;
+    long int aLast, bLast, cLast, dLast,temp1,temp2;
+    dLast = 500000;
+
     clock_t t;
     t = clock();
- 
-    for (a = 1; a < 100000; a++) {
-        for (b = 1; b < 300000; b++) {
-            for (c = 1; c < 500000; c++) {
-                for (d = 1; d < 500000; d++) {
+
+    for (d = 1; d < dLast; d++) {
+        cLast = d;
+
+        for (c = cLast; c > pow(pow(d, 4) - pow(c, 4), 0.25); c--) {
+            bLast = pow(pow(d, 4) - pow(c, 4), 0.25);
+
+            for (b = bLast; b > pow(pow(d, 4) - pow(c, 4) - pow(b, 4), 0.25) ; b--) {
+                aLast = pow(pow(d, 4) - pow(c, 4) - pow(b, 4), 0.25);
+           
+                for (a = aLast; a > 0; a--) {
                     if (prop(a, b, c, d))
                         printf("FOUND IT!\na = %ld\nb = %ld\nc = %ld\nd = %ld\n", a, b, c, d);
                 }
-                if (!(c%1000))
-                    printf("a = %ld, b = %ld, c = %ld, time = %fs\n", a, b, c, ((double)(clock() - t))/CLOCKS_PER_SEC);
             }
-            printf("a = %ld, b = %ld, time = %fs\n", a, b, ((double)(clock() - t))/CLOCKS_PER_SEC);
+            if (!(c % 1000))
+                printf(" d=%ld, c=%ld, time = %fs\n",d, c, ((double)(clock() - t)) / CLOCKS_PER_SEC);
         }
-        printf("a = %ld, time = %fs\n", a, ((double)(clock() - t))/CLOCKS_PER_SEC);
     }
- 
+
 }
